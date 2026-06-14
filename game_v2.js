577e15dcde22164c6bba780b5f4ae79abeed86e2
nacarrillor <nacarrillor@users.noreply.github.com>
2026-06-14 15:06:00 +0200

    Level 1: Reposition puddles so they do not overlap with rock sample positions, and bump version to v2.0.12

--- game_v2.js
+++ game_v2.js
@@ -1483,11 +1483,11 @@ function initLevel() {
         //
         // Obstacles ON HILLS only (far from rocks):
         puddles = [
-            { x: 1600, width: 60 },
+            { x: 1400, width: 60 },
             { x: 2525, width: 75 },
-            { x: 2750, width: 60 },
-            { x: 3950, width: 60 },
-            { x: 5150, width: 60 }
+            { x: 3050, width: 60 },
+            { x: 3800, width: 60 },
+            { x: 5000, width: 60 }
         ];
         hazards = [
             { x: 1325, y: getGroundHeight(1325) - 20, width: 25, height: 20, type: "geyser", isErupting: false, timer: 0 },
